3c85f5dcac2f4e76e4fbd6bc9f90d2c6f4d8a6f4
Daniel Rak <80216538+danielrak1@users.noreply.github.com>
2021-12-02 16:30:29 +0200

    [add] - added login, registration and list of users functionality

---
+++ src/main/java/com/example/endofgame/service/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.example.endofgame.service;
+
+import com.example.endofgame.entity.User;
+import com.example.endofgame.repository.UserRepository;
+import com.example.endofgame.vo.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(username);
+        if(user == null){
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new CustomUserDetails(user);
+    }
+}

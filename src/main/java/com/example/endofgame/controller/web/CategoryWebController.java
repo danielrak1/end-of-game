1464c409bfc2642a50014f19bc7ec985142572c5
Daniel Rak <80216538+danielrak1@users.noreply.github.com>
2021-12-18 09:54:12 +0200

    [add] - add_expense html page for adding expenses. [change] - working on connecting category and user tables.

--- src/main/java/com/example/endofgame/controller/web/CategoryWebController.java
+++ src/main/java/com/example/endofgame/controller/web/CategoryWebController.java
@@ -2,8 +2,13 @@ package com.example.endofgame.controller.web;
 
 import com.example.endofgame.dto.CategorySummary;
 import com.example.endofgame.entity.Category;
+import com.example.endofgame.entity.User;
+import com.example.endofgame.repository.CategoryRepository;
 import com.example.endofgame.service.CategoryService;
+import com.example.endofgame.service.CustomUserDetailsService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +20,13 @@ public class CategoryWebController {
 
     public static final String CATEGORIES_KEY = "categories";
     private final CategoryService categoryService;
+    private final CustomUserDetailsService detailsService;
+    private final CategoryRepository repository;
 
-    public CategoryWebController(CategoryService categoryService) {
+    public CategoryWebController(CategoryService categoryService, CustomUserDetailsService detailsService, CategoryRepository repository) {
         this.categoryService = categoryService;
+        this.detailsService = detailsService;
+        this.repository = repository;
     }
 
     @GetMapping("/all-categories")
@@ -47,5 +56,15 @@ public class CategoryWebController {
         model.addAttribute("category", new Category());
         return "add_category";
     }
+
+//    @PostMapping("/process_add-category")
+//    public String addCategory(Category category){
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        User user = detailsService.loadUser(auth.getName());
+//        category.setUser(user);
+//        repository.save(category);
+//
+//        return "redirect:/web/all-categories";
+//    }
 
 }
--- src/main/java/com/example/endofgame/service/CustomUserDetailsService.java
+++ src/main/java/com/example/endofgame/service/CustomUserDetailsService.java
@@ -1,13 +1,19 @@
 package com.example.endofgame.service;
 
+import com.example.endofgame.entity.Category;
 import com.example.endofgame.entity.User;
 import com.example.endofgame.repository.UserRepository;
 import com.example.endofgame.vo.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
@@ -21,4 +27,17 @@ public class CustomUserDetailsService implements UserDetailsService {
         }
         return new CustomUserDetails(user);
     }
+
+    public User loadUser(String username) throws UsernameNotFoundException{
+        User user = userRepo.findByEmail(username);
+
+        return user;
+    }
+
+//    @PostMapping("/process_add-category")
+//    public String addCategory(Category category){
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//
+//        User user =
+//    }
 }
